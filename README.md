cd36aa082abf126add1fb304953935787037882c
Adigboluja Ganiyat <devghandy@gmail.com>
2022-06-15 22:50:55 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+API_URL = "https://api.themoviedb.org/3/movie/popular?api_key=6939441d5cdb57b6582f8e3ecddebcb1"
+API_IMG = "https://image.tmdb.org/t/p/w500/"
+API_SEARCH = "https://api.themoviedb.org/3/search/movie?api_key=6939441d5cdb57b6582f8e3ecddebcb1&language=en-US&page=1&include_adult=false"
---
+++ src/components/Characters/CharacterGrid.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Spinner from '../ui/Spinner'
+import CharacterItems from './CharacterItems'
+
+const CharacterGrid = ({ items, isLoading }) => {
+  return isLoading ? (
+    <Spinner>Loading...</Spinner>
+    ) : (
+    <section className='cards'>
+        {items.map(item => (
+            <CharacterItems key={item.char_id} item={item}></CharacterItems>
+        ))}
+    </section>)
+}
+
+export default CharacterGrid
